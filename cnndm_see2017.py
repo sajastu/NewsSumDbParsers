a8e28fec3fc38bd5bc08706f39a29388f5ce9c3d
Sajad Sotudeh <sajad.sotudeh@gmail.com>
2019-12-02 18:02:20 -0500

    Quick updated cnndm.

--- cnndm_see2017.py
+++ cnndm_see2017.py
@@ -1,13 +1,15 @@
 """ This code is partially uses functions implemented by See et al."""
 
 import hashlib
+import json
 import os
 import sys
 
 import spacy
+from tqdm import tqdm
+
 spacy.prefer_gpu()
 nlp = spacy.load("en_core_web_sm")
-
 
 dm_single_close_quote = u'\u2019'  # unicode
 dm_double_close_quote = u'\u201d'
@@ -21,7 +23,7 @@ all_test_urls = "url_lists/all_test.txt"
 
 def read_txt_file(dir):
     lines = []
-    with open(dir, "r") as f:
+    with open(dir, "r", encoding='utf-8') as f:
         for line in f:
             lines.append(line.strip())
     return lines
@@ -30,7 +32,7 @@ def read_txt_file(dir):
 def hashhex(s):
     """Returns a heximal formated SHA1 hash of the input string."""
     h = hashlib.sha1()
-    h.update(s)
+    h.update(s.encode('utf-8'))
     return h.hexdigest()
 
 
@@ -81,46 +83,55 @@ def extract_sections(story_file):
 
 def preprocess_tokenize(file_names, cnn_dir, dm_dir):
     out = []
-    tmp={}
-    for s in file_names:
-        story_file = ' '
+    tmp = {}
+    for f_name in tqdm(file_names, total=len(file_names)):
+        story_file = ''
         # Look in the tokenized story dirs to find the .story file corresponding to this url
-        if os.path.isfile(os.path.join(cnn_dir, s)):
-            story_file = os.path.join(cnn_dir, s)
-        elif os.path.isfile(os.path.join(dm_dir, s)):
-            story_file = os.path.join(dm_dir, s)
+        if os.path.isfile(os.path.join(cnn_dir, f_name + '.story')):
+            story_file = os.path.join(cnn_dir, f_name + '.story')
+        elif os.path.isfile(os.path.join(dm_dir, f_name + '.story')):
+            story_file = os.path.join(dm_dir, f_name + '.story')
         else:
             print("Error: Couldn't find story file %s" % story_file)
 
-        article, abstract = extract_sections(story_file)
+        if len(story_file) > 0:
+            article, abstract = extract_sections(story_file)
 
-        doc_src = nlp(article, disable=['parser', 'tagger'])
-        doc_tgt = nlp(abstract, disable=['parser', 'tagger'])
-        src_arr = []
-        tgt_arr = []
-        for token_s in doc_src:
-            if len(token_s.text.strip()) > 0:
-                src_arr.append(token_s.text)
-        for token_tgt in doc_tgt:
-            if len(token_tgt.text.strip()) > 0:
-                tgt_arr.append(token_tgt.text)
+            doc_src = nlp(article, disable=['parser', 'tagger'])
+            doc_tgt = nlp(abstract, disable=['parser', 'tagger'])
+            src_arr = []
+            tgt_arr = []
+            for token_s in doc_src:
+                if len(token_s.text.strip()) > 0:
+                    src_arr.append(token_s.text)
+            for token_tgt in doc_tgt:
+                if len(token_tgt.text.strip()) > 0:
+                    tgt_arr.append(token_tgt.text)
 
-        tmp['text'] = src_arr.copy()
-        tmp['summary'] = tgt_arr.copy()
-        src_arr.clear()
-        tgt_arr.clear()
-        out.append(tmp.copy())
+            tmp['text'] = src_arr.copy()
+            tmp['summary'] = tgt_arr.copy()
+            src_arr.clear()
+            tgt_arr.clear()
+            out.append(tmp.copy())
+        else:
+            print('Did not added')
 
     return out
 
 
-def write_jsonl(data_arr, set):
-    ## TODO to be implemented
-    pass
+def write_jsonl(data_arr, set_name):
+    if not os.path.exists('/disk1/sajad/datasets/cnndm/tokenized/'):
+        os.mkdir('/disk1/sajad/datasets/cnndm/tokenized/')
+
+    out_file = open('/disk1/sajad/datasets/cnndm/tokenized/' + set_name + '.jsonl', mode='a')
+
+    for instance in tqdm(data_arr, total=len(data_arr)):
+        json.dump(instance, out_file)
+        out_file.write('\n')
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print("USAGE: python make_datafiles.py <cnn_stories_dir> <dailymail_stories_dir>")
+        print("USAGE: python cnndm_see2017.py <cnn_stories_dir> <dailymail_stories_dir>")
         sys.exit()
     cnn_stories_dir = sys.argv[1]
     dm_stories_dir = sys.argv[2]
